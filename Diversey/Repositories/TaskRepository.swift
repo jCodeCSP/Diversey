f6c3577533dd1ef8f8bac54fa3c0db07af49e0db
jCode <jcodecsp@gmail.com>
2021-06-21 10:35:08 -0500

    Integrated Firebase back end into the Task List page

---
+++ Diversey/Repositories/TaskRepository.swift
@@ -0,0 +1,60 @@
+//
+//  TaskRepository.swift
+//  Diversey
+//
+//  Created by Bryant Jaramillo on 6/20/21.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class TaskRepository: ObservableObject {
+    let db = Firestore.firestore()
+    
+    @Published var tasks = [Task]()
+    
+    init() {
+        loadData()
+    }
+    
+    func loadData() {
+        db.collection("tasks")
+          .order(by: "createdTime")
+            .addSnapshotListener { (querySnapshot, error) in
+            if let querySnapshot = querySnapshot {
+                self.tasks = querySnapshot.documents.compactMap { document in
+                    do {
+                   let x = try document.data(as: Task.self)
+                        return x
+                    }
+                    catch {
+                        print(error)
+                    }
+                    return nil
+                }
+            }
+        }
+    }
+    func addTask(_  task:  Task){
+        do{
+            let _ = try db.collection("tasks").addDocument(from: task)
+        }
+    catch {
+            fatalError("Unable to encode task: \(error.localizedDescription)")
+        }
+    }
+    
+    func updateTask(_ task: Task){
+        if let taskID = task.id{
+            do{
+                try db.collection("tasks").document(taskID).setData(from: task)
+            }
+            catch {
+                fatalError("Unable to encode task: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    
+}
